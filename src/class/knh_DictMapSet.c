1390086605e079f80a47986f198d51c811af77af
imasahiro <imasahiro@413261d9-973d-0410-9b72-d5665a44c00b>
2010-04-12 12:46:52 +0000

    fix knh_DictMap_remove remove map element
    git-svn-id: http://svn.sourceforge.jp/svnroot/konoha/trunk/konoha@1281 413261d9-973d-0410-9b72-d5665a44c00b

--- src/class/knh_DictMapSet.c
+++ src/class/knh_DictMapSet.c
@@ -542,10 +542,13 @@ void knh_DictMap_set(Ctx *ctx, knh_DictMap_t *o, knh_String_t *key, Any *v)
 void knh_DictMap_remove(Ctx *ctx, knh_DictMap_t *o, knh_String_t *key)
 {
 	int loc = knh_dict_index_b(o->_list, o->size, o->fcmp, __tobytes(key));
-	if(loc != 1) {
+	if(loc != -1) {
 		KNH_SETv(ctx, o->list[loc].key, key);  /* To avoid losing key */
 		KNH_SETv(ctx, o->list[loc].value, KNH_NULL);
+		o->size -= 1;
 	}
+	fprintf(stderr, "%p,%d\n",o->_list, o->size);
+	knh_dict_sort(o->_list, o->size);
 }
 
 /* ------------------------------------------------------------------------ */
