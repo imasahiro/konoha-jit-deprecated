ddfd3018324ee89b880ece63dae66f6624ce7cd5
imasahiro <imasahiro@413261d9-973d-0410-9b72-d5665a44c00b>
2010-04-15 08:49:10 +0000

    remove uuid function
    git-svn-id: http://svn.sourceforge.jp/svnroot/konoha/trunk/konoha@1294 413261d9-973d-0410-9b72-d5665a44c00b

--- src/labs/check.c
+++ src/labs/check.c
@@ -1,7 +1,4 @@
 #include "commons.h"
-#if defined(KNH_USING_POSIX)
-#include <uuid/uuid.h>
-#endif
 
 #ifdef __cplusplus
 extern "C" {
@@ -16,90 +13,41 @@ extern "C" {
 #else
 #define CPU_NAME "unknown"
 #endif
-
 #define UPDATE_SERVER "http://www.ubicg.ynu.ac.jp/cgi-bin/konoha-userdata/update_server"
-#define UUID_FILE  ".konoha/knh.uuid"
 /*--------------------------------------------------------------*/
-static void setUUID(char *buffer)
+static void knh_check_exec(void *arg)
 {
-#if defined(KNH_USING_POSIX)
-	uuid_t uuid;
-	uuid_generate(uuid);
-	uuid_unparse(uuid, buffer);
-#elif defined(KNH_USING_WINDOWS)
-	/* TODO  Imprement uuid for Windows */
-	knh_snprintf(buffer, 64, "I_like_Windows");
-#else
-	knh_snprintf(buffer, 64, "unknown");
-#endif
-}
-
-/*--------------------------------------------------------------*/
-static void send_user_record(Ctx *ctx, char *uuid)
-{
+	Ctx *ctx = (Ctx*)arg;
 	char url[512] = {0};
-	knh_snprintf(url, 512, "%s?arch=%s(%s)&rev=%d&user_id=%s", 
+	knh_snprintf(url, 512, "%s?arch=%s(%s)&rev=%d", 
 			UPDATE_SERVER, KONOHA_PLATFORM,
-			CPU_NAME, KONOHA_REVISION, uuid);
+			CPU_NAME, KONOHA_REVISION);
 
 	knh_iodrv_t *d = knh_getIODriver(ctx, STEXT("http"));
 	knh_io_t fd = d->fopen(ctx, B(url), "r", 0);
 	if (fd != -1) {
 		d->fread(ctx, fd, url, 512);
+		d->fclose(ctx, fd);
 	}
-	d->fclose(ctx, fd);
-}
-
-/*--------------------------------------------------------------*/
-static void knh_check_exec(void *arg)
-{
-	Ctx *ctx = (Ctx*)arg;
-	char filename[FILEPATH_BUFSIZ] = {0};
-	char uuid[64] = {0};
-	knh_iodrv_t *d;
-	knh_io_t fp;
-
-	knh_snprintf(filename, FILEPATH_BUFSIZ, 
-			"%s/%s", knh_getenv("HOME"), UUID_FILE);
-
-	d = knh_getIODriver(ctx, STEXT("file"));
-	if ((fp = d->fopen(ctx, B(filename), "r", 0)) != -1) {
-		d->fread(ctx, fp, uuid, 64);
-	} else {
-		setUUID(uuid);
-		fp = d->fopen(ctx, B(filename), "w", 0);
-		// write uuid to file
-		d->fwrite(ctx, fp, uuid, 64);
-	}
-	d->fclose(ctx, fp);
-	send_user_record(ctx, uuid);
 }
 
 /*--------------------------------------------------------------*/
 void knh_check_update(Ctx *ctx)
 {
-	char filename[FILEPATH_BUFSIZ] = {0};
 	knh_io_t fp;
 	knh_iodrv_t *d;
 	knh_thread_t th;
+	threadfunc_t func = knh_check_exec;
 
-	knh_snprintf(filename, FILEPATH_BUFSIZ,
-			"%s/%s", knh_getenv("HOME"), UUID_FILE);
-
-	d = knh_getIODriver(ctx, STEXT("file"));
-	if ((fp = d->fopen(ctx, B(filename), "r", 0)) != -1) {
-		d->fclose(ctx, fp);
-	} else {
-		if (!knh_ask(ctx, 
-					"Do you mind to send your environment information "
-					"to update_server? [y/N]", 0)) {
-			return;
-		}
+	if (!knh_ask(ctx, "Do you mind to send your environment information "
+				"to update_server? [y/N]", 0)) {
+		return;
 	}
 
-	knh_thread_create(ctx, &th, NULL, knh_check_exec, (void*)ctx);
+	knh_thread_create(ctx, &th, NULL, func, (void*)ctx);
 	knh_thread_detach(ctx, th);
 }
+/*--------------------------------------------------------------*/
 
 #ifdef __cplusplus
 }
