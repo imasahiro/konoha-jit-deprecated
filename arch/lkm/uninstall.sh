582ca600ac04135d6e830b1dfada84cf640e0a87
imasahiro <imasahiro@users.sourceforge.jp>
2011-05-11 15:01:26 +0900

    remove old files

--- arch/lkm/uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd ./debug/
-sh ./uninstall.sh
-cd ../
-echo "c.unregister();" > /dev/konoha
-sudo rmmod konohadev
--- ktest.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-konoha -t ktest/class/object.ktest ktest/class/boolean.ktest ktest/class/int.ktest ktest/class/float.ktest
